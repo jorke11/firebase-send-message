7a910762b9540d8cbdff3680d593ae06d0655837
Jorge Pinedo <jorke8710@gmail.com>
2019-07-06 16:38:21 -0500

    Initial Commit

---
+++ FirebaseSendMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSendMessage
+//
+//  Created by Jorge Pinedo on 7/6/19.
+//  Copyright © 2019 Jorge Pinedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
