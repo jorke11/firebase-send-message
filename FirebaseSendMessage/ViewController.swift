a7f79a598e400c7c9f0d8a9f9f554122267b1bfc
Jeisson Monroy <jorke8710@gmail.com>
2021-03-26 08:46:54 -0500

    add

--- FirebaseSendMessage/ViewController.swift
+++ FirebaseSendMessage/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var input: UITextField!
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
+        let ref = Database.database().reference()
+
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ref.child("message").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? String
+//            let username = value?["username"] as? String ?? ""
+//            let user = User(username: username)
+            print(value)
+            self.label.text = value
+            
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
 
-
+    @IBAction func send(_ sender: Any) {
+        
+        let ref = Database.database().reference()
+        ref.child("message").setValue(input.text)
+        
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-FirebaseSendMessage/Pods-FirebaseSendMessage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseSendMessage : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseSendMessage
+@end
